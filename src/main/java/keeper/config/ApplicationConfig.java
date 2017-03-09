80b299bc02a9fb09462d0f6e8ac33fa6831000a3
GoodforGod <goodforgod.dev@gmail.com>
2017-03-09 22:51:27 +0300

    - Init

---
+++ src/main/java/keeper/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package keeper.config;
+
+/*
+ * Created by GoodforGod 
+ * Date: 09.03.2017
+ * Time: 22:49
+ */
+
+/*
+ * Main Application Spring Configuration
+ */
+public class ApplicationConfig {
+
+}
