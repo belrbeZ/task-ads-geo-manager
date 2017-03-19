e6ffb37ab1c75b54223b4443cbff17c144aae850
GoodforGod <goodforgod.dev@gmail.com>
2017-03-20 01:44:46 +0300

    - POM upgrades - Utils converter skeleton

--- util/src/main/java/com/keeper/Converter.java
+++ util/src/main/java/com/keeper/Converter.java
@@ -4,10 +4,38 @@ package com.keeper;
  * Created by GoodforGod on 19.03.2017.
  */
 
+import com.keeper.entity.*;
+
+import java.util.Locale;
+
 /**
  * Default Comment
  */
 public class Converter
 {
 
+    public String convertToJson(User usr) {
+
+        return "";
+    }
+
+    public String converToJson(Task task) {
+
+        return "";
+    }
+
+    public String convertToJson(Route route) {
+
+        return "";
+    }
+
+    public String convertToJson(Coordinate coord) {
+
+        return "";
+    }
+
+    public String convertToJson(Location location) {
+
+        return "";
+    }
 }
