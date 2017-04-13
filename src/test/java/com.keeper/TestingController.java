0fd9cb104f08fe349f6acb56a3561ed9c1d9e914
Alexandr Vasilev <alexandrvasilievby@gmail.com>
2017-04-13 06:04:16 +0300

    Welcome page for ui.

---
+++ src/test/java/com.keeper/TestingController.java
@@ -0,0 +1,33 @@
+package com.keeper;
+
+import com.keeper.service.test.UserTestRepoService;
+import junit.framework.TestCase;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by Alexandr Vasiliev on 11.04.2017.
+ *
+ * @author Alexandr Vasiliev
+ */
+public class TestingController extends TestCase {
+
+    private final UserTestRepoService repoService;
+
+    @Autowired
+    public TestingController(UserTestRepoService repoService) {
+        this.repoService = repoService;
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testApp() throws Exception {
+        System.out.println(repoService.get(1L).getEmail());
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
