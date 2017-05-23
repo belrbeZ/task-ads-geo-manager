3e4a106341d9d446dc9d06741692a6cb68be04e1
GoodforGod <goodforgod.dev@gmail.com>
2017-05-24 00:20:19 +0300

    - Auto Login

--- src/main/java/com.keeper/controllers/web/SecureWebController.java
+++ src/main/java/com.keeper/controllers/web/SecureWebController.java
@@ -7,6 +7,7 @@ package com.keeper.controllers.web;
  *
  */
 
+import com.keeper.model.dao.User;
 import com.keeper.model.dto.UserFormDTO;
 import com.keeper.service.modelbased.impl.UserService;
 import com.keeper.util.ModelTranslator;
@@ -15,8 +16,11 @@ import com.keeper.util.resolvers.WebResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -27,6 +31,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.Optional;
 
 /**
  * Control Web & Api OAUTH and Registration forms
@@ -38,9 +43,24 @@ public class SecureWebController {
 
     private final UserService userService;
 
+    private final AuthenticationManager authenticationManager;
+
     @Autowired
-    public SecureWebController(UserService userService) {
+    public SecureWebController(UserService userService, AuthenticationManager authenticationManager) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    private void authenticateUserAndSetSession(User user, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+
+        // generate session if one doesn't exist
+        request.getSession();
+
+        token.setDetails(new WebAuthenticationDetails(request));
+        Authentication authenticatedUser = authenticationManager.authenticate(token);
+
+        SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
     }
 
     @RequestMapping(value = WebResolver.LOGIN, method = RequestMethod.GET)
@@ -69,20 +89,27 @@ public class SecureWebController {
     }
 
     @RequestMapping(value = WebResolver.REGISTER, method = RequestMethod.POST)
-    public ModelAndView registrationPost(@Valid UserFormDTO user, BindingResult bindingResult) {
-        ModelAndView modelAndView = new ModelAndView(TemplateResolver.LOGIN);
+    public ModelAndView registrationPost(@Valid UserFormDTO user, BindingResult bindingResult, HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView(TemplateResolver.redirect(WebResolver.FEED));
 
         if(userService.existsByEmail(user.getEmail())) {
-            bindingResult.rejectValue("email",
-                    "error.user",
+            bindingResult.rejectValue("email", "error.user",
                     "There is already a user registered with the email provided");
             modelAndView.setViewName(TemplateResolver.REGISTER);
         }
 
         if (!bindingResult.hasErrors()) {
-            userService.save(ModelTranslator.toDAO(user));
-            modelAndView.addObject("successMessage", "User has been registered successfully");
-            modelAndView.addObject("user", new UserFormDTO());
+            try {
+                Optional<User> usr = userService.save(ModelTranslator.toDAO(user));
+
+                if(!usr.isPresent())
+                    throw new NullPointerException();
+
+                authenticateUserAndSetSession(usr.get(), request);
+            } catch (Exception e) {
+                e.printStackTrace();
+                modelAndView.addObject("message", "Error");
+            }
         }
 
         return modelAndView;
